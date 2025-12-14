a3976eead93056542a1d88ab8546275c26fd0fe3
Michael Francis <michaelmugendi1707@gmail.com>
2025-12-14 18:13:14 +0300

    Update: project learned

--- script/deploy.py
+++ script/deploy.py
@@ -1,5 +1,6 @@
 from src import favorites
 from moccasin.boa_tools import VyperContract
+from moccasin.config import get_active_account
 
 def deploy_favorites() -> VyperContract:
     print("Deploying the application...")
@@ -7,6 +8,14 @@ def deploy_favorites() -> VyperContract:
     starting_number: int = favorites_contract.retrieve()
     print(f"Favorites contract deployed at {favorites_contract.address} with starting number {starting_number}.")
     print("Deployment complete.")
+
+    active_network = get_active_account()
+    if active_network.has_explorer:
+        print("Verifying contract on explorer...")
+        result = active_network.moccasin_veriry(favorites_contract)
+        result.wait_for_verification()
+        print("Verification complete.")
+
     return favorites_contract
 
 def moccasin_main():
